8d2a3df209449e5846ca473a19a0249583d1c831
fionafrm <fiona.ratu@ui.ac.id>
2023-11-21 23:21:53 +0700

    try deploy

--- lib/screens/shoplist_form.dart
+++ lib/screens/shoplist_form.dart
@@ -128,7 +128,7 @@ class _ShopFormPageState extends State<ShopFormPage> {
                               // Kirim ke Django dan tunggu respons
                               // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
                               final response = await request.postJson(
-                              "http://fiona-ratu-tutorial.pbp.cs.ui/create-flutter/",
+                              "http://fiona-ratu-tutorial.pbp.cs.ui.ac.id/create-flutter/",
                               jsonEncode(<String, String>{
                                   'name': _name,
                                   'price': _price.toString(),
